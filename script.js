d9004ce798768c452aed03ad9e4a6a955171c610
Wilkson Flor <wilksonflor12@gmail.com>
2023-11-25 02:09:07 -0300

    andamento do desafio

---
+++ script.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('.menu')
+
+window.addEventListener('scroll', menufixo);
+
+function menufixo() {
+  const nav = document.querySelector('.menu');
+  if (window.scrollY > 0) {
+    nav.classList.add('fixednav');
+  } else {
+    nav.classList.remove('fixednav');
+  }
+}
+
+
+function animateHeaderTransition() {
+    requestAnimationFrame(menufixo);
+  }
+  window.addEventListener("scroll", animateHeaderTransition);
+  
+  menufixo();
+  window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+  });
+  
